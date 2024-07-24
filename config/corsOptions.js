ae26a788caa60306ca9bff41600c703bff42f691
RomeroArias23 <137248494+RomeroArias23@users.noreply.github.com>
2024-07-24 14:32:13 -0600

    server update

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,9 +1,10 @@
+const cors = require('cors');
+
 const corsOptions = {
-    origin: 'https://yellow-project.onrender.com',  
-    optionsSuccessStatus: 200,  
-    credentials: true,  
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE']  
-  };
-  
-  module.exports = corsOptions;
+    origin: ['https://yellow-project-api.onrender.com', 'https://yellow-project.onrender.com', 'http://localhost:3000'], // Add other domains as needed
+    credentials: true, // if you use cookies/session information
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+module.exports = corsOptions;
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const app = require('./app');
 const connectDB = require('./config/db');
 
 connectDB().then(() => {
-  const port = process.env.PORT;
+  const port = process.env.PORT || 3000;
   app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+      console.log(`Server started on port ${port}`);
   });
+}).catch(error => {
+  console.error("Database connection failed", error);
+  process.exit(1);  // Optional: Exit the process if DB connection fails
 });
