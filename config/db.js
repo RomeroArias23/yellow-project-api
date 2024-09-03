52722d5b8575e2213e1c039eed700e4f30279bcc
RomeroArias23 <137248494+RomeroArias23@users.noreply.github.com>
2024-09-02 19:18:25 -0600

    connection update

--- config/db.js
+++ config/db.js
@@ -6,12 +6,38 @@ const mongoURL = process.env.MONGO_URL;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoURL);
+    await mongoose.connect(mongoURL, {
+      useNewUrlParser: true,           
+      useUnifiedTopology: true,        
+      poolSize: 10,                    
+      connectTimeoutMS: 30000,        
+      socketTimeoutMS: 45000,          
+      keepAlive: true,                
+      keepAliveInitialDelay: 300000,   
+      serverSelectionTimeoutMS: 5000,  
+      autoReconnect: true,             
+      reconnectTries: Number.MAX_VALUE, 
+      reconnectInterval: 5000          
+    });
+
     console.log('Database connected!');
   } catch (error) {
-    console.error(error);
-    process.exit(1); // Exit process with failure
+    console.error('Error connecting to the database:', error);
+    process.exit(1); 
   }
+
+  // Handling disconnection
+  mongoose.connection.on('disconnected', () => {
+    console.log('MongoDB disconnected. Attempting to reconnect...');
+  });
+
+  mongoose.connection.on('reconnected', () => {
+    console.log('MongoDB reconnected!');
+  });
+
+  mongoose.connection.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+  });
 };
 
 module.exports = connectDB;
